90b7a83cdef5a93e4fdbd6a43910adf98b1d65d2
agusbudoyo <asbudoyo@gmail.com>
2023-10-05 11:50:42 +0700

    bootstrap

---
+++ src/component/navBar.jsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <><h1>Header Ini Bootstrap</h1><button type="button" class="btn btn-primary">Button</button></>
+    )
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './component/navBar';
+import * as bootstrap from 'bootstrap';
+import './css/styles.scss';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
 )
